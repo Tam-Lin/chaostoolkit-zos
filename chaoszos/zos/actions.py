769a6d3d5284ce37c2a7b8da93dba56e74410d6a
Kevin McKenzie <kmckenzi@us.ibm.com>
2022-05-04 18:04:12 -0400

    Get basic configure tests working
    Signed-off-by: Kevin McKenzie <kmckenzi@us.ibm.com>

--- chaoszos/zos/actions.py
+++ chaoszos/zos/actions.py
@@ -5,14 +5,16 @@ from chaoslib.exceptions import ActivityFailed, InterruptExecution
 from chaoslib.types import Configuration, Secrets
 
 from logzero import logger
-import re
+
+from zos_utilities.lpar import LPAR, LPARException
 
 from chaoszos.__send_zos_command import Send_Command
 
 __all__ = ["configure_processors"]
 
-def configure_processors(configuration: Configuration = None, secrets: Secrets = None, processor_type: str = None,
-                         processor_count: int = None, status: str = None, location: str = None):
+def configure_processors(configuration: Configuration = None, secrets: Secrets = None,
+                         processor_type_to_change: str = None,
+                         processor_count_to_change: int = None, status: str = None, location: str = None):
 
     """
     Configures processors either offline or online, depending on the action
@@ -22,20 +24,26 @@ def configure_processors(configuration: Configuration = None, secrets: Secrets =
 
     :param configuration:
     :param secrets:
-    :param processor_type: Type of processors to configure offline.  Can be None, cp, or ziip
-    :param processor_count:  The number of processors to configure; if None, defaults to all processors of that type
+    :param processor_type_to_change: Type of processors to configure offline.  Can be None, cp, or ziip
+    :param processor_count_to_change:  The number of processors to configure; if None, defaults to all processors of that type
     :param status:  Intended final configuration (offline or online)
     :param location:  The z/OS image you want to configure processors
     :return:
     """
 
-    if processor_type is not None and processor_type != "ziip" and processor_type != "cp":
+    logger.debug("Configuration: %s" % configuration)
+    logger.debug("processor_type_to_change: %s" % processor_type_to_change)
+    logger.debug("processor_count_to_change: %s" % processor_count_to_change)
+    logger.debug("status: %s" % status)
+    logger.debug("location: %s" % location)
+
+    if processor_type_to_change is not None and processor_type_to_change != "ziip" and processor_type_to_change != "cp":
         raise InterruptExecution("Invalid processor type specified")
 
     if status != "online" and status != "offline":
         raise InterruptExecution("Status must be online or offline")
 
-    if processor_count is None and (processor_type is None or processor_type == "cp") and status == "offline":
+    if processor_count_to_change is None and (processor_type_to_change is None or processor_type_to_change == "cp") and status == "offline":
         raise InterruptExecution("Can not configure all CPs offline")
 
     if location is None or location is "":
@@ -46,39 +54,29 @@ def configure_processors(configuration: Configuration = None, secrets: Secrets =
     except KeyError:
         raise InterruptExecution(location + " not found in secrets")
 
-    core_re = re.compile(
-        "(?P<coreid>[0-9A-F]{4})  (?P<wlmmanaged>.)(?P<online>.)(?P<type>.)  (?P<lowcp>[0-9A-F]{4})-(?P<highcp>[0-9A-F]{4})(  (?P<polarity>.)(?P<parked>.)  (?P<subclassmask>[0-9A-F]{4})  (?P<state1>.)(?P<state2>.))?")
+    #As of V2R3, I *think* (need to check) D M=CORE will work either way.  Need to check.
 
-    core_list = list()
+    test_lpar = LPAR()
 
-    print(len(dmcore.message_out), flush=True)
+    test_lpar.parse_d_m_core(dmcore.message_out)
 
-    for line in dmcore.message_out[3:]:
+    processors_remaining_to_change = processor_count_to_change
 
-        print(line, flush=True)
+    logger.debug("Changing %s prcoessors of type %s to %s" % (processors_remaining_to_change, processor_type_to_change, status))
 
-        core_info = core_re.search(line)
+    for (processor_name, processor) in test_lpar.logical_processors.items():
 
-        print(core_info, flush=True)
+        logger.debug(processor)
 
-        if core_info is None:
+        if (processor_type_to_change == "ziip" and processor.type == "zIIP") or (processor_type_to_change == "cp" and processor.type == "CP"):
+            if (status == "offline" and processor.online == True) or (status == "online" and processor.online == False):
+                logger.info("Configuring CORE " + processor.coreid + " " + status.upper())
+                configure_command = Send_Command(location, secrets[location], "CF CORE(" + processor.coreid + ")," + status.upper(), "IEE505I")
+
+                if processors_remaining_to_change is not None:
+                    processors_remaining_to_change = processors_remaining_to_change - 1
+
+                    logger.debug("%s processors left to change" % (processors_remaining_to_change))
+
+        if processors_remaining_to_change is 0:
             break
-        else:
-            core = dict()
-            core["coreid"] = core_info.group("coreid")
-            core["type"] = core_info.group("type")
-            core["online"] = core_info.group("online")
-
-            core_list.append(core)
-
-    #really can clean you up; need to deal with CORE and CPU variations
-    for core in core_list:
-
-        logger.debug(core)
-        if (processor_type == "ziip" and core["type"] == "I") or (processor_type == "cp" and core["type"] == "C"):
-            if (status == "offline" and core["online"] == "+") or (status == "online" and core["online"] == "-"):
-                logger.info("Configuring CORE " + core["coreid"] + " " + status.upper())
-                configure_command = Send_Command(location, secrets[location], "CF CORE(" + core["coreid"] + ")," + status.upper(), "IEE505I")
-
-
-
--- tests/zos/test_zos_actions.py
+++ tests/zos/test_zos_actions.py
@@ -16,6 +16,79 @@ def test_configure_all_ziip_cores_offline(send_command):
     send_command_1 = MagicMock()
     send_command_2 = MagicMock()
     send_command_3 = MagicMock()
+    send_command_4 = MagicMock()
+    send_command_5 = MagicMock()
+
+    send_command_1.message_out = [
+'IEE174I 14.12.45 DISPLAY M 781                   ',
+'CORE STATUS: HD=Y   MT=1                         ',
+'ID    ST   ID RANGE   VP  ISCM  CPU THREAD STATUS',
+'0000   +   0000-0000  H   FC00  +                ',
+'0001   +   0001-0001  M   0000  +                ',
+'0002   +   0002-0002  LP  0000  +                ',
+'0003   +   0003-0003  LP  0000  +                ',
+'0004   +   0004-0004  LP  0000  +                ',
+'0005   +   0005-0005  LP  0000  +                ',
+'0006   +I  0006-0006  H   0200  +                ',
+'0007   +I  0007-0007  H   0200  +                ',
+'0008   +I  0008-0008  H   0200  +                ',
+'0009   +I  0009-0009  H   0200  +                ',
+'000A   -I  000A-000A                             ',
+'000B   -I  000B-000B                             ',
+'000C   -I  000C-000C                             ',
+'000D   -I  000D-000D                             ',
+'000E   -I  000E-000E                             ',
+'000F   -I  000F-000F                             ',
+        '',
+'CPC ND = 008562.T02.IBM.02.0000000790A8                       ',
+'CPC SI = 8562.Z06.IBM.02.00000000000790A8                     ',
+'         Model: T02                                          ',
+'CPC ID = 00                                                   ',
+'CPC NAME = T256                                               ',
+'LP NAME = S5E        LP ID = 21                               ',
+'CSS ID  = 2                                                   ',
+'MIF ID  = 1                                                   ',
+'                                                             ',
+'+ ONLINE    - OFFLINE    N NOT AVAILABLE    / MIXED STATE    ',
+'W WLM-MANAGED                                                 ',
+'                                                             ',
+'I        INTEGRATED INFORMATION PROCESSOR (zIIP)              ',
+'CPC ND  CENTRAL PROCESSING COMPLEX NODE DESCRIPTOR            ',
+'CPC SI  SYSTEM INFORMATION FROM STSI INSTRUCTION              ',
+'CPC ID  CENTRAL PROCESSING COMPLEX IDENTIFIER                 ',
+'CPC NAME CENTRAL PROCESSING COMPLEX NAME                      ',
+'LP NAME  LOGICAL PARTITION NAME                               ',
+'LP ID    LOGICAL PARTITION IDENTIFIER                         ',
+'CSS ID   CHANNEL SUBSYSTEM IDENTIFIER                         ',
+'MIF ID   MULTIPLE IMAGE FACILITY IMAGE IDENTIFIER             '
+        ]
+
+    send_command_2.message_out = ['IEE505I CORE(06),OFFLINE']
+    send_command_3.message_out = ['IEE505I CORE(07),OFFLINE']
+    send_command_4.message_out = ['IEE505I CORE(08),OFFLINE']
+    send_command_5.message_out = ['IEE505I CORE(09),OFFLINE']
+
+    send_command.side_effect = [send_command_1, send_command_2, send_command_3, send_command_4, send_command_5]
+
+    configure_processors(processor_type_to_change='ziip', status='offline',
+                         location="SYS1", secrets=secrets)
+
+    # assert send_command.call_count == 3
+
+    send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="D M=CORE", message_to_watch_for="IEE174I")
+    send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="CF CORE(0006),OFFLINE", message_to_watch_for="IEE505I")
+    send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="CF CORE(0007),OFFLINE", message_to_watch_for="IEE505I")
+    send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="CF CORE(0008),OFFLINE", message_to_watch_for="IEE505I")
+    send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="CF CORE(0009),OFFLINE", message_to_watch_for="IEE505I")
+
+@patch('chaoszos.zos.actions.Send_Command', autospec=True)
+def test_configure_1_ziip_core_offline(send_command):
+
+    secrets = dict()
+    secrets["SYS1"] = "password1"
+
+    send_command_1 = MagicMock()
+    send_command_2 = MagicMock()
 
     send_command_1.message_out = [
 'IEE174I 16.54.16 DISPLAY M 923                   ',
@@ -36,44 +109,59 @@ def test_configure_all_ziip_cores_offline(send_command):
 '000C   +   0018-0019  H   0000  +N               ',
 '000D   +   001A-001B  H   0000  +N               ',
 '000E   +I  001C-001D  H   0000  ++               ',
-'000F   +I  001E-001F  H   0000  ++               '
-        ]
+'000F   +I  001E-001F  H   0000  ++               ',
+        '',
+        'CPC ND = 008562.T02.IBM.02.0000000790A8                       ',
+        'CPC SI = 8562.Z06.IBM.02.00000000000790A8                     ',
+        '         Model: T02                                          ',
+        'CPC ID = 00                                                   ',
+        'CPC NAME = T256                                               ',
+        'LP NAME = S5E        LP ID = 21                               ',
+        'CSS ID  = 2                                                   ',
+        'MIF ID  = 1                                                   ',
+        '                                                             ',
+        '+ ONLINE    - OFFLINE    N NOT AVAILABLE    / MIXED STATE    ',
+        'W WLM-MANAGED                                                 ',
+        '                                                             ',
+        'I        INTEGRATED INFORMATION PROCESSOR (zIIP)              ',
+        'CPC ND  CENTRAL PROCESSING COMPLEX NODE DESCRIPTOR            ',
+        'CPC SI  SYSTEM INFORMATION FROM STSI INSTRUCTION              ',
+        'CPC ID  CENTRAL PROCESSING COMPLEX IDENTIFIER                 ',
+        'CPC NAME CENTRAL PROCESSING COMPLEX NAME                      ',
+        'LP NAME  LOGICAL PARTITION NAME                               ',
+        'LP ID    LOGICAL PARTITION IDENTIFIER                         ',
+        'CSS ID   CHANNEL SUBSYSTEM IDENTIFIER                         ',
+        'MIF ID   MULTIPLE IMAGE FACILITY IMAGE IDENTIFIER             '
+
+    ]
 
     send_command_2.message_out = ['IEE505I CORE(0E),OFFLINE']
 
-    send_command_3.message_out = ['IEE505I CORE(0F),OFFLINE']
+    send_command.side_effect = [send_command_1, send_command_2]
 
-    send_command.side_effect = [send_command_1, send_command_2, send_command_3]
+    configure_processors(processor_type_to_change='ziip', status='offline', location="SYS1", secrets=secrets,
+                         processor_count_to_change=1)
 
-    configure_processors(processor_type='ziip', status='offline', location="SYS1", secrets=secrets)
-
-    # assert send_command.call_count == 3
+    assert send_command.call_count == 2
 
     send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="D M=CORE", message_to_watch_for="IEE174I")
     send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="CF CORE(000E),OFFLINE", message_to_watch_for="IEE505I")
-    send_command.assert_any_call(location="SYS1", connection_information="password1", command_to_send="CF CORE(000F),OFFLINE", message_to_watch_for="IEE505I")
 
-# def test_configure_cpu_offline(send_command_init):
-#
-#     send_command_init.return_value = True
-#
-#     configure_processors(processor_type='ziip', status='offline', location="SYS1")
-
-def test_configure_all_cps_offline():
+def test_attempt_to_configure_all_cps_offline():
     with pytest.raises(InterruptExecution) as x:
-        configure_processors(processor_type="cp", status='offline', location="SYS1")
+        configure_processors(processor_type_to_change="cp", status='offline', location="SYS1")
     assert 'Can not configure all CPs offline' in str(x.value)
 
-def test_configure_invalid_processor_type():
+def test_configure_invalid_processor_type_to_change():
     with pytest.raises(InterruptExecution) as x:
-        configure_processors(processor_type="bob", status='offline', location="SYS1")
+        configure_processors(processor_type_to_change="bob", status='offline', location="SYS1")
     assert 'Invalid processor type specified' in str(x.value)
 
 def test_configure_empty_location_offline():
     with pytest.raises(InterruptExecution) as x:
-        configure_processors(processor_type="ziip", status='offline', location="")
+        configure_processors(processor_type_to_change="ziip", status='offline', location="")
     assert 'No target specified for action' in str(x.value)
 
 # def test_configure_empty_secrets_offline():
 #     with pytest.raises(KeyError) as x:
-#         configure_processors(processor_type='ziip', status='offline', location='SYS1')
+#         configure_processors(processor_type_to_change='ziip', status='offline', location='SYS1')
